ad3a21456cc34777d124f57aa6379f5344c8d1bc
zcq <zcq960921@163.com>
2019-04-24 00:55:00 +0800

    mybatis generator

---
+++ himall/src/main/java/com/zcq/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.zcq.dao;
+
+import com.zcq.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
---
+++ himall/src/main/java/com/zcq/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zcq.dao;
+
+import com.zcq.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
