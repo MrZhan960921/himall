3cf7dd2304672a1aa521712a8922974a69a8544b
zcq <zcq960921@163.com>
2019-04-25 20:25:47 +0800

    收货地址模块

---
+++ himall/src/main/java/com/zcq/controller/portal/ShippingController.java
@@ -0,0 +1,100 @@
+package com.zcq.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+
+import com.zcq.common.Const;
+import com.zcq.common.ResponseCode;
+import com.zcq.common.ServerResponse;
+import com.zcq.pojo.Shipping;
+import com.zcq.pojo.User;
+import com.zcq.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by geely
+ */
+
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+
+    @Autowired
+    private IShippingService iShippingService;
+
+
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(),shipping);
+    }
+
+
+    @RequestMapping("del.do")
+    @ResponseBody
+    public ServerResponse del(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.del(user.getId(),shippingId);
+    }
+
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session,Shipping shipping){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(),shipping);
+    }
+
+
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse<Shipping> select(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(),shippingId);
+    }
+
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize",defaultValue = "10")int pageSize,
+                                         HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(),pageNum,pageSize);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ himall/src/main/java/com/zcq/service/IShippingService.java
@@ -0,0 +1,19 @@
+package com.zcq.service;
+
+import com.github.pagehelper.PageInfo;
+import com.zcq.common.ServerResponse;
+import com.zcq.pojo.Shipping;
+
+
+/**
+ * Created by geely
+ */
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+    ServerResponse<String> del(Integer userId, Integer shippingId);
+    ServerResponse update(Integer userId, Shipping shipping);
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+
+}
