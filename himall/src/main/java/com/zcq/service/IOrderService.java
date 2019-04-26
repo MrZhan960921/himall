524ec6d6718575093b27b81bce15ed13c305c312
zcq <zcq960921@163.com>
2019-04-26 21:45:35 +0800

    订单功能开发,批量插入sql

--- himall/src/main/java/com/zcq/service/IOrderService.java
+++ himall/src/main/java/com/zcq/service/IOrderService.java
@@ -1,6 +1,8 @@
 package com.zcq.service;
 
+import com.github.pagehelper.PageInfo;
 import com.zcq.common.ServerResponse;
+import com.zcq.vo.OrderVo;
 
 import java.util.Map;
 
@@ -14,4 +16,10 @@ public interface IOrderService {
     ServerResponse aliCallback(Map<String,String> params);
 
     ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+
+    ServerResponse createOrder(Integer userId,Integer shippingId);
+    ServerResponse<String> cancel(Integer userId,Long orderNo);
+    ServerResponse getOrderCartProduct(Integer userId);
+    ServerResponse<OrderVo> getOrderDetail(Integer userId, Long orderNo);
+    ServerResponse<PageInfo> getOrderList(Integer userId, int pageNum, int pageSize);
 }
